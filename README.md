a31b4ecc3a5b8d266f4c68e703eb55fa9ef9317d
box-monkey <jr.gasparin15@gmail.com>
2022-05-14 02:31:29 -0400

    initial set up

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # rpg-companion
+little app to add some utility to table top rpgs
---
+++ script.js
@@ -0,0 +1,16 @@
+// global variables
+function myFunction() {
+    console.log("js linked")
+}
+
+console.log(myFunction())
+
+// die randomizer functions
+
+
+
+// item quality randomizer function
+
+
+
+// encounter randomizer functions
